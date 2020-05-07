18ac575f9c5782d6597cbd17a357252cf78aca86
mlplab <labpc2019@gmail.com>
2020-05-07 13:12:13 +0900

    repear train

--- train.py
+++ train.py
@@ -4,7 +4,8 @@ import os
 import torch
 # import torchvision
 from trainer import Trainer
-from model.HIPN import HSI_Network
+# from model.HIPN import HSI_Network
+from model.test_model import Attention_HSI_Model
 from data_loader import PatchMaskDataset
 # from utils import RandomCrop, RandomHorizontalFlip
 from utils import ModelCheckPoint
@@ -22,23 +23,20 @@ if device == 'cuda':
 
 data_name = 'CAVE'
 img_path = 'dataset/'
-train_path = os.path.join(img_path, 'train')
-test_path = os.path.join(img_path, 'test')
+train_path = os.path.join(img_path, 'train_patch_data')
+test_path = os.path.join(img_path, 'test_patch_data')
 ckpt_path = 'ckpt'
 # drive_path = '/content/drive/My Drive/auto_colorization/'
 
-train_list = os.listdir(train_path)
-test_list = os.listdir(test_path)
-# transform = (RandomCrop(crop), RandomHorizontalFlip(),
-#              torchvision.transform.ToTensor())
-train_transform = None
+train_transform = (RandomHorizontalFlip(), torchvision.transform.ToTensor())
+# train_transform = None
 # test_transform = (RandomHorizontalFlip(), torchvision.transforms.ToTensor())
 test_transform = None
 train_dataset = PatchMaskDataset(train_path, os.path.join(img_path, 'mask_data'), tanh=False, transform=train_transform)
 train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
 test_dataset = PatchMaskDataset(test_path, os.path.join(img_path, 'mask_data'), tanh=False, transform=test_transform)
 test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-model = HSI_Network(1, 31).to(device)
+model = Attention_HSI_Model(1, 31).to(device)
 criterion = torch.nn.MSELoss().to(device)
 param = list(model.parameters())
 optim = torch.optim.Adam(lr=1e-3, params=param)
