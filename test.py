d636948d287cc6761eac198830ed756252dc83df
mlplab <labpc2019@gmail.com>
2020-06-22 21:19:07 +0900

    research cave bands

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ func_name = transformations.RGB_10_degree_cmfs_to_XYZ_10_degree_cmfs
 start_wave = 400
 last_wave = 700
 # data_name = 'dataset/icvl/mat/icvl.mat'
-data_name = 'dataset/train/balloons_ms.mat'
+data_name = 'dataset/test/stuffed_toys_ms.mat'
 x = np.arange(start_wave, last_wave + 1, 10)
 
 
@@ -24,7 +24,6 @@ data = normalize(f['im'])
 print(data.shape)
 trans_filter = func_name(x)
 for i, ch in enumerate(range(start_wave, last_wave + 1, 10)):
-     break
      print(i, ch)
      # trans_data = normalize(np.expand_dims(data[:, :, 10], axis=-1).dot(np.expand_dims(trans_filter[10], axis=0)))
      trans_data = normalize(np.expand_dims(data[:, :, i], axis=-1).dot(np.expand_dims(trans_filter[i], axis=0)))
@@ -36,8 +35,8 @@ for i, ch in enumerate(range(start_wave, last_wave + 1, 10)):
      plt.imsave(f'output_img/{i}_{ch}.png', show_data)
      plt.close()
      # plt.show()
-g_ch = 16
-b_ch = 9
+g_ch =13
+b_ch =6
 for i in range(21, 31):
      show_data = normalize(data[:, :, (i, g_ch, b_ch)])
      # show_data = trans_data
