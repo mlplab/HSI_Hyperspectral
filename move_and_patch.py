15a1ad8f01c4d3b629ea83c69ddc9bdf90fadb76
mlplab <labpc2019@gmail.com>
2020-06-25 17:45:55 +0900

    repear create mask

--- move_and_patch.py
+++ move_and_patch.py
@@ -4,6 +4,7 @@
 import os
 import random
 import shutil
+import scipy.io
 import numpy as np
 from tqdm import tqdm
 from utils import make_patch, patch_mask
@@ -35,9 +36,6 @@ mask_show_path = os.path.join(save_path, 'mask_show_data')
 data_key = {'CAVE': 'im', 'Harvard': 'ref', 'ICVL': 'data'}
 data_size = {'CAVE': (512, 512, 31), 'Harvard': (1040, 1392, 31), 'ICVL': (1392, 1300, 31)}
 seed_key = {'CAVE': 1, 'Harvard': 2, 'ICVL': 3}
-train_test_idx = {'CAVE': np.array([1] * 20 + [2] * 12),
-                  'Harvard': np.array([1] * 40 + [2] * 10),
-                  'ICVL': np.random.choice((1, 2), data_list.shape[0], p=(.8, .2))}
 np.random.seed(seed_key[data_name])
 
 
@@ -52,11 +50,15 @@ os.makedirs(save_path, exist_ok=True)
 mask_data = np.random.choice((0., 1.),
                              (data_size[data_name][0] + datasize[data_name][2], data_size[data_name][1]),
                              p=(.5, .5))
-mask_data = np.array([mask_data[i: datad_size[data_name][0]] + i] for i in range(data_size[data_name][-1])).transpose(1, 2, 0)
+mask_data = np.array([mask_data[i: datad_size[data_name][0] + i] for i in range(data_size[data_name][-1])]).transpose(1, 2, 0)
 scipy.io.savemat(os.path.join(save_path, 'mask.mat'), {'data': mask_data})
 data_list = os.listdir(data_path)
 data_list.sort()
 data_list = np.array(data_list)
+np.random.seed(seed_key[data_name])
+train_test_idx = {'CAVE': np.array([1] * 20 + [2] * 12),
+                  'Harvard': np.array([1] * 40 + [2] * 10),
+                  'ICVL': np.random.choice((1, 2), data_list.shape[0], p=(.8, .2))}
 train_list = list(data_list[train_test_idx[data_name] == 1])
 test_list = list(data_list[train_test_idx[data_name] == 2])
 print(len(train_list), len(test_list))
@@ -70,9 +72,9 @@ make_patch(test_data_path, eval_path, size=256, step=256, ch=31, data_key=data_k
 make_patch(test_data_path, eval_show_path, size=512, step=512, ch=31, data_key=data_key[data_name])
 
 
-patch_mask(os.path.join(data_path, 'mask.mat'), mask_path, size=64, step=64, ch=31)
-patch_mask(os.path.join(data_path, 'mask.mat'), eval_mask_path, size=256, step=256, ch=31)
-patch_mask(os.path.join(data_path, 'mask.mat'), mask_show_path, size=512, step=512, ch=31)
+patch_mask(os.path.join(save_path, 'mask.mat'), mask_path, size=64, step=64, ch=31)
+patch_mask(os.path.join(save_path, 'mask.mat'), eval_mask_path, size=256, step=256, ch=31)
+patch_mask(os.path.join(save_path, 'mask.mat'), mask_show_path, size=512, step=512, ch=31)
 
 
 callback_list = os.listdir(eval_show_path)
