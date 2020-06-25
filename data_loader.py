15296b4613dd72666d8a4274663d9ba0bfb5343a
mlplab <labpc2019@gmail.com>
2020-06-25 17:17:31 +0900

    remove callback_dataset

--- data_loader.py
+++ data_loader.py
@@ -39,8 +39,6 @@ class HyperSpectralDataset(torch.utils.data.Dataset):
         trans_data = nd_data
         label_data = trans_data
         measurement_data = (trans_data * self.mask).sum(dim=0, keepdim=True)
-        if self.tanh is True:
-            label_data = torch.tanh(label_data)
         if self.concat is True:
             input_data = torch.cat([measurement_data, self.mask], dim=0)
         else:
@@ -82,8 +80,6 @@ class PatchMaskDataset(torch.utils.data.Dataset):
         mask = sio.loadmat(os.path.join(self.mask_path, f'mask_{patch_id}.mat'))[self.data_key]
         mask = self.mask_transforms(mask)
         measurement_data = (trans_data * mask).sum(dim=0, keepdim=True)
-        if self.tanh is True:
-            label_data = torch.tanh(label_data)
         if self.concat is True:
             input_data = torch.cat([measurement_data, mask], dim=0)
         else:
@@ -110,35 +106,8 @@ class PatchEvalDataset(PatchMaskDataset):
         mask = sio.loadmat(os.path.join(self.mask_path, f'mask_{patch_id}.mat'))[self.data_key]
         mask = self.mask_transforms(mask)
         measurement_data = (trans_data * mask).sum(dim=0, keepdim=True)
-        if self.tanh is True:
-            label_data = torch.tanh(label_data)
         if self.concat is True:
             input_data = torch.cat([measurement_data, mask], dim=0)
         else:
             input_data = measurement_data
         return self.data[idx], input_data, label_data
-
-
-class CallbackDataset(PatchMaskDataset):
-
-    def __getitem__(self, idx):
-        patch_id = self.data[idx].split('.')[0].split('_')[-1]
-        mat_data = sio.loadmat(os.path.join(self.img_path, self.data[idx]))[self.data_key]
-        nd_data = np.array(mat_data, dtype=np.float32).copy()
-        if self.transforms is not None:
-            for transform in self.transforms:
-                nd_data = transform(nd_data)
-        else:
-            nd_data = torchvision.transforms.ToTensor()(nd_data)
-        trans_data = nd_data
-        label_data = trans_data
-        mask = sio.loadmat(os.path.join(self.mask_path, f'mask_{patch_id}.mat'))[self.data_key]
-        mask = self.mask_transforms(mask)
-        measurement_data = (trans_data * mask).sum(dim=0, keepdim=True)
-        if self.tanh is True:
-            label_data = torch.tanh(label_data)
-        if self.concat is True:
-            input_data = torch.cat([measurement_data, mask], dim=0)
-        else:
-            input_data = measurement_data
-        return input_data, label_data
