38a767a8cd00a17cecf083ce4ccf35250658cdcb
mlplab <labpc2019@gmail.com>
2020-07-05 18:57:48 +0900

    shared residual block

--- model/dense_net.py
+++ model/dense_net.py
@@ -18,7 +18,8 @@ class Dense_HSI_prior_Network(torch.nn.Module):
         stack_num = output_ch * block_num
         self.start_conv = torch.nn.Conv2d(input_ch, output_ch, 3, 1, 1)
         self.start_shortcut = torch.nn.Identity()
-        self.residual_block = torch.nn.ModuleList([torch.nn.Conv2d(output_ch, output_ch, 3, 1, 1) for _ in range(block_num)])
+        # self.residual_block = torch.nn.ModuleList([torch.nn.Conv2d(output_ch, output_ch, 3, 1, 1) for _ in range(block_num)])
+        self.residual_block = torch.nn.Conv2d(output_ch, output_ch, 1, 1, 0)
         self.hsi_block = torch.nn.ModuleList([HSI_prior_block(output_ch, output_ch, feature=feature, activation=activation) for _ in range(block_num)])
         self.dense_conv = torch.nn.ModuleList([torch.nn.Conv2d(output_ch + output_ch, output_ch, 3, 1, 1) for _ in range(block_num)])
         # self.output_conv = torch.nn.Conv2d(output_ch, output_ch, 1, 1, 0)
@@ -28,8 +29,8 @@ class Dense_HSI_prior_Network(torch.nn.Module):
         x = self.start_conv(x)
         x_in = self.start_shortcut(x)
         x_stack = []
-        for residual, hsi, dense_conv in zip(self.residual_block, self.hsi_block, self.dense_conv):
-            x_res = residual(x)
+        for hsi, dense_conv in zip(self.hsi_block, self.dense_conv):
+            x_res = self.residual_block(x)
             x_hsi = hsi(x)
             x = x_res + x_hsi
             x = torch.cat((x, x_in), dim=1)
