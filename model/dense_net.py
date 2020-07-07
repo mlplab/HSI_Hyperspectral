5f71babe8bbc58435b903f2f94712b926d32df6c
mlplab <labpc2019@gmail.com>
2020-07-07 15:36:29 +0900

    delete start_shortcut

--- model/dense_net.py
+++ model/dense_net.py
@@ -17,17 +17,14 @@ class Dense_HSI_prior_Network(torch.nn.Module):
             feature = 64
         stack_num = output_ch * block_num
         self.start_conv = torch.nn.Conv2d(input_ch, output_ch, 3, 1, 1)
-        self.start_shortcut = torch.nn.Identity()
-        # self.residual_block = torch.nn.ModuleList([torch.nn.Conv2d(output_ch, output_ch, 3, 1, 1) for _ in range(block_num)])
         self.residual_block = torch.nn.Conv2d(output_ch, output_ch, 1, 1, 0)
         self.hsi_block = torch.nn.ModuleList([HSI_prior_block(output_ch, output_ch, feature=feature, activation=activation) for _ in range(block_num)])
         self.dense_conv = torch.nn.ModuleList([torch.nn.Conv2d(output_ch + output_ch, output_ch, 3, 1, 1) for _ in range(block_num)])
-        # self.output_conv = torch.nn.Conv2d(output_ch, output_ch, 1, 1, 0)
         self.output_conv = torch.nn.Conv2d(stack_num, output_ch, 1, 1, 0)
 
     def forward(self, x):
         x = self.start_conv(x)
-        x_in = self.start_shortcut(x)
+        x_in = x
         x_stack = []
         for hsi, dense_conv in zip(self.hsi_block, self.dense_conv):
             x_res = self.residual_block(x)
